01693d12da56404366ee1aab529d98cce7c9ab78
surfing-girl <joanna.demecka@gmail.com>
2017-03-23 17:49:56 +0000

    Clear the code

--- js/app.js
+++ js/app.js
@@ -6,22 +6,16 @@ function Pagination(studentsNumberOnPage, className) {
 	this.displayListLi = [];
 	for (var i = 0; i < this.studentsListLi.length; i++) {
 		this.displayListLi.push(i);
-	};	
+	};
 	this.index = 0;
 	var pagination = document.createElement('div');
 	pagination.className = 'pagination';
 	this.unorderedList = document.createElement('ul');
-	var container = document.getElementsByClassName('page')[0]; 
+	var container = document.getElementsByClassName('page')[0];
 
 	container.appendChild(pagination);
 	pagination.appendChild(this.unorderedList);
-	for (var i = 0; i < this.studentsListLi.length; i++) {
-		var span = document.createElement('span');
-		span.innerText = i + 1;
-		this.studentsListLi[i].appendChild(span);
-	}
-
-}
+};
 
 Pagination.prototype.printPagination = function() {
 	var printNumbers = '';
@@ -31,10 +25,9 @@ Pagination.prototype.printPagination = function() {
 	if (lastPage > 0) {
 		pages += 1;
 	};
-	console.log(lastPage + ' ' + pages);
 	for (var i = 1; i <= pages; i++) {
 		printNumbers += '<li><a href="#">';
-		printNumbers += i + '</a></li>';		
+		printNumbers += i + '</a></li>';
 	};
 	this.unorderedList.innerHTML = printNumbers;
 };
@@ -42,15 +35,13 @@ Pagination.prototype.printPagination = function() {
 Pagination.prototype.showStudents = function(pageValue) {
 	this.index = this.studentsNumberOnPage * pageValue;
 	for (var i = 0; i < this.studentsListLi.length; i++) {
-		this.studentsListLi[i].style.display = 'none';	
-	};	
+		this.studentsListLi[i].style.display = 'none';
+	};
 	for (var i = 0; i < this.displayListLi.length; i++) {
 		if (i >= this.index - this.studentsNumberOnPage && i < this.index) {
-			this.studentsListLi[this.displayListLi[i]].style.display = 'block';	
-		}
+			this.studentsListLi[this.displayListLi[i]].style.display = 'block';
+		};
 	};
-	console.log('Index: '+ this.index);
-	console.log('StudentsNumberOnPage: '+ this.studentsNumberOnPage + ' ,pageValue: ' + pageValue);
 };
 
 var newPagination = new Pagination(10, 'student-item');
@@ -76,12 +67,10 @@ Pagination.prototype.paginationClickHandler = function (element, elementValue) {
 	element.onclick = function (event) {
 		event.preventDefault();
 		newPagination.showStudents(elementValue + 1);
-		newPagination.ulFadeIn();	
+		newPagination.ulFadeIn();
 	};
 };
 
 
 newPagination.showStudents(1);
 newPagination.createAnchorList().forEach(newPagination.paginationClickHandler, newPagination);
-
-
--- js/searchBar.js
+++ js/searchBar.js
@@ -1,16 +1,24 @@
 'use strict';
 
-function SearchBar (tag, pagination) {
-	this.studentsNamesTags = document.getElementsByTagName(tag);
+function SearchBar (pagination) {
+	var studentsNamesTags = document.getElementsByTagName('h3');
+	var studentsEmailsClasses = document.getElementsByClassName('email');
+	var studentListContainer = document.getElementsByClassName('student-list')[0];
 	this.studentsNamesList = [];
+	this.studentsEmailsList = [];
 	this.inputSearch = document.getElementsByTagName('input')[0];
 	this.button = document.getElementById('button');
 	this.searchValue = '';
-	this.searchList = [];
 	this.pagination = pagination;
+	this.noStudentMessage = document.createElement('div');
+	studentListContainer.appendChild(this.noStudentMessage);
+	this.noStudentMessage.innerHTML = '';
 
-	for (var i = 0; i < this.studentsNamesTags.length; i++) {
-		this.studentsNamesList.push(this.studentsNamesTags[i].innerText);
+	for (var i = 0; i < studentsNamesTags.length; i++) {
+		this.studentsNamesList.push(studentsNamesTags[i].innerText);
+	};
+	for (var i = 0; i < studentsEmailsClasses.length; i++) {
+		this.studentsEmailsList.push(studentsEmailsClasses[i].innerText);
 	};
 }
 
@@ -19,27 +27,43 @@ SearchBar.prototype.submission = function () {
 	var index = 0;
 	this.pagination.displayListLi = [];
 	for (var i = 0; i < this.studentsNamesList.length; i++) {
-		if (this.studentsNamesList[i].toLowerCase().indexOf(this.searchValue.toLowerCase()) >= 0) {
+		if (this.studentsNamesList[i].toLowerCase().indexOf(this.searchValue.toLowerCase()) >= 0 || this.studentsEmailsList[i].toLowerCase().indexOf(this.searchValue.toLowerCase()) >= 0) {
 			this.pagination.displayListLi.push(i);
 			index++;
 		}
 	};
 	if (index == 0) {
-		var studentListContainer = document.getElementsByClassName('student-list')[0];
-		studentListContainer.innerHTML = '<h1>There is no <span class="alert">' + this.searchValue + '</span> in our databse.';
+		this.noStudentMessage.innerHTML = '<h1>There is no <span class="alert">' + this.searchValue + '</span> in our databse.'
+	} else {
+		this.noStudentMessage.innerHTML = '';
 	};
 	this.pagination.printPagination();
-	console.log('displayListLi LENGHT: ' + this.pagination.displayListLi.length);
 }
 
-SearchBar.prototype.clickHandler = function () {
+SearchBar.prototype.eventHandler = function () {
 	this.submission();
 	this.pagination.showStudents(1);
 	this.pagination.createAnchorList().forEach(this.pagination.paginationClickHandler, this.pagination);
 	this.pagination.ulFadeIn();
 }
 
-var newSearchBar = new SearchBar('h3', newPagination);
-button.addEventListener("click", function() { newSearchBar.clickHandler(); }, false);
+var newSearchBar = new SearchBar(newPagination);
+//button.addEventListener("click", function() { newSearchBar.eventHandler(); }, false);
+
+if (document.getElementById('button').addEventListener) {
+    document.getElementById('button').addEventListener("click", function() { newSearchBar.eventHandler(); }, false);
+}
+else {
+    document.getElementById('button').attachEvent("onclick", newSearchBar.eventHandler);
+}
+
+$(document).ready(function() {
+	$('#search').keyup(function () {
+		newSearchBar.eventHandler();
+	});
+
+});
+
+
 
 
